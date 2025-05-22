8e538591e83a39dbd22d249b1b80dba2797f8ee9
joao <joao.projex@gmail.com>
2025-05-22 19:54:24 -0400

    Implemented liquibase for database migrations

--- src/main/java/com/reringuy/support/services/TokenService.java
+++ src/main/java/com/reringuy/support/services/TokenService.java
@@ -1,13 +1,11 @@
 package com.reringuy.support.services;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jwt;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import javax.crypto.SecretKey;
 import java.util.Date;
 
